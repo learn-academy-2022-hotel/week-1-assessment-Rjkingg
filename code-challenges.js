79e6a15f5aef474c212b40a88591b5d43e8e9a9e
Rjkingg <kingraq1007@gmail.com>
2022-12-05 08:45:45 -0800

    completed assesment

--- code-challenges.js
+++ code-challenges.js
@@ -17,10 +17,29 @@ const fruit2 = "banana"
 const fruit3 = "cherry"
 const fruit4 = "kiwi"
 // Expected outcome: "cherry"
-
+// START
 // --------------------1) Create a function that takes in a number and determines if the given number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below.
 
-// Pseudo code:
+// Pseudo code: 
+
+// To solve this challenge, I know that I will need to use a function. I need to establish a  keyword. Because we have multiple variables, it will be an "if else" Statement. 
+// keyword: "let" 
+// Next, I need a condition to put into the parenthesis- I will use (boilingPoint) because it is a detailed description. Inside each of my curly brackets contains a code block. All of the contents in the code block are related to the keyword contain an action, if ran. 
+
+let boilingPoint = 212
+
+if(boilingPoint===212) {
+
+console.log("212 is at boiling point")
+
+} else if(boilingPoint===42) {
+   
+console.log("42 is below boiling point")
+
+} else if (boilingPoint===350){
+  
+    console.log("350 is above boiling point")
+}
 
 const temp1 = 42
 // Expected output: "42 is below boiling point"
@@ -33,22 +52,31 @@ const temp3 = 212
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the test variables provided below.
 
-// Pseudo code:
-
+// Pseudo code: 
+// The best way to get the length of an Array is to use the .length property. We can also assign a variable to that .length. We also need to use the .concat method to combine the two arrays.
 const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
 const padres1998WorldSeriesRuns = [6, 3, 5, 3]
-// Expected output: 9
+const padres1998WorldSeries = padres1984WorldSeries.concat(padres1998WorldSeries.length)
+
+
+// const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
+// const padres1998WorldSeriesRuns = [6, 3, 5, 3]
+// // Expected output: 9
 
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below.
 
-// Pseudo code:
+// Pseudo code: 
+// The .reverse method is used to reverse letters. I will ned to use console.log to view the output. The variable has a name of "currentCohort" and it needs the .reverse in order to perform the action:
+console.log(currentCohort.reverse)
 
 const currentCohort = "Hotel 2022"
 // Expected output: "2202 letoH"
 
 // --------------------4) Create the code that will return the last index of the given value from the array using the test variables provided below.
 
-// Pseudo code:
+// Pseudo code: Looking at the elements in the array, I see that it has has an index of 8. It starts from 0, (13) and ends at 8 (10). The givenValue1 is set to 42, which lets me know that they're using [7]. 
+
+// console.log(myArray[7]) is what I was thinking but that would give an output of 42, instead of 7. 
 
 const myNumbers = [13, 34, 42, 5, 5, 10, 27, 42, 10]
 const givenValue1 = 42
@@ -59,7 +87,10 @@ const givenValue2 = 10
 
 // --------------------5) Create the code that will sort all the numbers in an array from largest to smallest. Use the test variables provided below.
 
-// Pseudo code:
+// Pseudo code: .sort method is used to compare values.
+
+console.log(typeof dataTypes.length) and then add the .sort method
+console.log(sanDiegoSummerTemperature.sort)
 
 const sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76]
 // Expected output: [82, 80, 79, 77, 76, 73, 72]
